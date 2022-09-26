f38b63e29203da82c2b6ba06fa6ebaafd570108d
MicTheObnoxious <85187759+MicTheObnoxious@users.noreply.github.com>
2022-09-26 14:53:25 +0100

    2

--- input_handlers.py
+++ input_handlers.py
@@ -4,11 +4,12 @@ import tcod.event
 
 from actions import Action, EscapeAction, MovementAction
 
+
 class EventHandler(tcod.event.EventDispatch[Action]):
-    def ev_quit(self, event: tcod.event.quit) -> Optional[Action]:
+    def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]:
         raise SystemExit()
     
-    def ev_keydown(self, event:tcod.event.KeyDown) -> Optional[Action]:
+    def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[Action]:
         action: Optional[Action] = None
         
         key = event.sym 
